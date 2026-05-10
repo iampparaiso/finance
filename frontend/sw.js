699616ad89b5669046fe9427926f146d533f90e2
Paulo Paraiso <Paulo.Paraiso@chubb.com>
2026-05-11 02:41:34 +0800

    fix: add skipWaiting/clients.claim to SW, fix manifest orientation and maskable icon

--- frontend/sw.js
+++ frontend/sw.js
@@ -20,13 +20,14 @@ const PRECACHE = [
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
