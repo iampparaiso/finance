740b2225e06be07434ae5d40ef116135b788291f
Paulo Paraiso <Paulo.Paraiso@chubb.com>
2026-05-11 03:51:29 +0800

    fix: update Apps Script URL to v2 deployment (Anyone access)

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbzSda4-Y6T9WVxyD6UrhjVyI-9CIOL8p6DSvAORymx6qI_fbEnAQRqrF5LIHWYx1Q/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycby2RpfT6ghC5Wz0HkQIU1bX5WFya_se9_OMmblyUZk5Z1IUcf44W5UPUvbf_Gy_2oV0/exec';
